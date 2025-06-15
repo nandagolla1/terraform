2e3b706d33b763b64f691f83ac456bf80755114c
nanda <nandagolla074@gmail.com>
2025-06-15 19:41:46 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,33 +1,29 @@
 resource "aws_instance" "roboshop" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
-  tags = {
-    Name = "HelloWorld"
-  }
+  tags = var.instance_name
 }
 
 resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all traffic"
+  name        = var.sg_name
+  description = var.sg_description
 
-  tags = {
-    Name = "allow_all"
-  }
+  tags = var.sg_tag
 
   ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port       = var.ingress_from_port
+    to_port         = var.ingress_to_port
+    protocol        = var.sg_protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = var.ipv6_cidr_blocks
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port       = var.egress_from_port
+    to_port         = var.egress_to_port
+    protocol        = var.sg_protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = var.ipv6_cidr_blocks
   }
 }
