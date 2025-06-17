18b00341986c8d63ba9e24bb0a958e0ccac83c7d
nanda <nandagolla074@gmail.com>
2025-06-17 09:44:02 +0530

    terraform

---
+++ for-loop/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "roboshop" {
+  count = 6
+  ami           = var.ami_id
+  instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  tags = {
+    Name = var.instances_loop[count.index]
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = var.sg_name
+  description = var.sg_description
+
+  tags = var.sg_tag
+
+  ingress {
+    from_port       = var.ingress_from_port
+    to_port         = var.ingress_to_port
+    protocol        = var.sg_protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = var.ipv6_cidr_blocks
+  }
+
+  egress {
+    from_port       = var.egress_from_port
+    to_port         = var.egress_to_port
+    protocol        = var.sg_protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = var.ipv6_cidr_blocks
+  }
+}
